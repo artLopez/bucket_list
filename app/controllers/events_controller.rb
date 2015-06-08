7bd7beb99b74d30bb677586b9edd74bae6d5b3f8
artLopez <arlopez@csumb.edu>
2015-06-08 10:29:59 -0700

    Created the events page to create,show event and all events

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,24 @@
+class EventsController < ApplicationController
+
+  def create
+    @event = Event.new(event_params)
+
+    @event.save
+    redirect_to @event
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def index
+    @event = Event.all
+  end
+
+
+  private
+  def event_params
+    params.require(:events).permit(:title,:location,:date,:description)
+  end
+
+end
