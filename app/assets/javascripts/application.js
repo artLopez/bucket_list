91ad3bd861560e4e4fe1949b02b88ad3d15689fe
Tenji Tembo <tenji240@gmail.com>
2015-06-13 23:20:07 -0700

    Started Porting my ideas and updates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,33 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jqueryEasingMin
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+$("#menu-toggle").click(function(e) {
+    console.log('yeap');
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
