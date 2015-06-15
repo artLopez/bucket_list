c16824bb60fb968ea864b14d49306767518db141
Kaitlyn Carter <kaitlyncarter@Tenjis-MacBook-Air.local>
2015-06-15 16:25:01 -0700

    #2 - adjusted padding and images

--- db/schema.rb
+++ db/schema.rb
@@ -27,8 +27,6 @@ ActiveRecord::Schema.define(version: 20150608161906) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
     t.string   "username"
     t.string   "password"
     t.datetime "created_at", null: false
